95d501a74d5c6e4bab24d680e62cb73d779509ed
notdls <nunya@hacking.lol>
2021-09-04 22:25:22 +1000

    Fix some minor grammar/spelling mistakes, add contributing section to readme

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 ## grayurls
 ------
 
-A common workflow when performing reconaissance on a target domain or web application is identifying potential paths or content, this is commonly performed by retreiving indexed URLs from services such as archive.org and Common Crawl using tools like [waybackurls](https://github.com/tomnomnom/waybackurls) and [gau](https://github.com/lc/gau). This tool is inspired by them and very similar, retrieving URLs that have once been shortened by a URL shortening service (e.g. goo.gl, bit.ly, etc.) using [GrayhatWarfare's API](https://shorteners.grayhatwarfare.com/docs/api/v1). They currently have a database of ~1.1 billion resolved short URLs, which may contain sensitive or otherwise interesting paths or endpoints. 
+A common workflow when performing reconnaissance on a target domain or web application is identifying potential paths or content, this is commonly performed by retrieving indexed URLs from services such as archive.org and Common Crawl using tools like [waybackurls](https://github.com/tomnomnom/waybackurls) and [gau](https://github.com/lc/gau). This tool is inspired by them and very similar, retrieving URLs that have once been shortened by a URL shortening service (e.g. goo.gl, bit.ly, etc.) using [GrayhatWarfare's API](https://shorteners.grayhatwarfare.com/docs/api/v1). They currently have a database of ~1.1 billion resolved short URLs, which may contain sensitive or otherwise interesting paths or endpoints. 
 
 ## Limitations
 To utilise this tool you will need to have a valid API key for the GrayhatWarfare API, [registration](https://grayhatwarfare.com/register) is free and will provide you access to the first ~5,000 results for any given domain/subdomain. However, any records beyond the 4,000 mark will require a premium subscription.
 
 ## Usage & Examples
+Setting API key environment variable
+```
+export GRAYHATWARFARE_API_KEY=key
+```
 Examples
 ```
 $ grayurls --target google.com
@@ -25,3 +29,6 @@ optional arguments:
   -d DEBUG, --debug DEBUG
                         Print debug information
 ```
+
+## Contributing 
+Issues, contributions, pull requests, etc, all welcome.
--- grayurls.py
+++ grayurls.py
@@ -10,7 +10,7 @@ def get_urls(target, api_key):
     # get initial resp
     response = s.get("https://shorteners.grayhatwarfare.com/api/v1/subdomain/{}/files/0/1?access_token={}".format(target, api_key)).json()
     url_count = response["urlsCount"]
-    logging.debug("{} available for {}, {} requests are required, scraping..".format(url_count, target, int(url_count/1000)+1))
+    logging.debug("{} available for {}, {} requests are required, grabbing now".format(url_count, target, int(url_count/1000)+1))
     # scrape all the urls and print to stdout
     for i in range(0, url_count, 1000):
         try:
@@ -21,7 +21,7 @@ def get_urls(target, api_key):
                     print(url["url"])
             elif "error" in batch:
                 if "Results listing is limited to protect resources." in batch["error"]:
-                    logging.debug("Scraping stopped due to API_KEY access cap, to get around this you will need to upgrade your account")
+                    logging.debug("Scraping stopped due to API limitations, to get around this you will need to upgrade your account")
                     break
                 else:
                     logging.debug("Unknown response received from server: {}".format(batch["error"]))
@@ -32,7 +32,7 @@ def get_urls(target, api_key):
 
 def main():
     # Parse Arguments
-    parser = argparse.ArgumentParser(description="Scraped shortned URLs for a given site from the GrayHatWarefare Shorteners API")
+    parser = argparse.ArgumentParser(description="Grabs shortned URLs for a given site from the GrayHatWarfare Shorteners API")
     parser.add_argument('-t','--target', type=str, action="store", dest="target", help="Target domain/subdomains (e.g. example.com)", required=True)
     parser.add_argument('-d','--debug', type=bool, action="store", dest="debug", help="Print debug information", default=False)
     args = parser.parse_args()
@@ -51,7 +51,7 @@ def main():
     if os.getenv("GRAYHATWARFARE_API_KEY"):
         api_key = os.getenv("GRAYHATWARFARE_API_KEY")
     else:
-        print("An API key is required to use this script, please register to obtain one at https://grayhatwarfare.com/register")
+        print("An API key is required to use this script, please register to obtain one at https://grayhatwarfare.com/register. If you already have one, please set your API key using environment variables, e.g. export GRAYHATWARFARE_API_KEY=key")
         exit()
 
     # setup debug logging if requested
