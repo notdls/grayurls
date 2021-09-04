d7d3da961031a1b56f6f9ebf117726257ce0cf47
notdls <nunya@hacking.lol>
2021-09-04 22:22:54 +1000

    Add script and project files, initial commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# grayurls
-CLI tool to obtain a list of resolved short URLs for given domain using GrayhatWarfare's Shorteners API. 
+## grayurls
+------
+
+A common workflow when performing reconaissance on a target domain or web application is identifying potential paths or content, this is commonly performed by retreiving indexed URLs from services such as archive.org and Common Crawl using tools like [waybackurls](https://github.com/tomnomnom/waybackurls) and [gau](https://github.com/lc/gau). This tool is inspired by them and very similar, retrieving URLs that have once been shortened by a URL shortening service (e.g. goo.gl, bit.ly, etc.) using [GrayhatWarfare's API](https://shorteners.grayhatwarfare.com/docs/api/v1). They currently have a database of ~1.1 billion resolved short URLs, which may contain sensitive or otherwise interesting paths or endpoints. 
+
+## Limitations
+To utilise this tool you will need to have a valid API key for the GrayhatWarfare API, [registration](https://grayhatwarfare.com/register) is free and will provide you access to the first ~5,000 results for any given domain/subdomain. However, any records beyond the 4,000 mark will require a premium subscription.
+
+## Usage & Examples
+Examples
+```
+$ grayurls --target google.com
+$ grayurls -t google.com
+```
+Usage
+```
+usage: grayurls.py [-h] -t TARGET [-d DEBUG]
+
+Scraped shortned URLs for a given site from the GrayHatWarefare Shorteners API
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -t TARGET, --target TARGET
+                        Target domain/subdomains (e.g. example.com)
+  -d DEBUG, --debug DEBUG
+                        Print debug information
+```
---
+++ grayurls.py
@@ -0,0 +1,66 @@
+import requests
+import argparse
+import os
+import logging
+import sys
+
+def get_urls(target, api_key):
+    s = requests.Session()
+    s.headers.update({"User-Agent":"grayurls (https://github.com/notdls/grayurls)"})
+    # get initial resp
+    response = s.get("https://shorteners.grayhatwarfare.com/api/v1/subdomain/{}/files/0/1?access_token={}".format(target, api_key)).json()
+    url_count = response["urlsCount"]
+    logging.debug("{} available for {}, {} requests are required, scraping..".format(url_count, target, int(url_count/1000)+1))
+    # scrape all the urls and print to stdout
+    for i in range(0, url_count, 1000):
+        try:
+            batch = s.get("https://shorteners.grayhatwarfare.com/api/v1/subdomain/{}/files/{}/1000?access_token={}".format(target, i, api_key)).json()
+            #print(batch)
+            if "urls" in batch:
+                for url in batch["urls"]:
+                    print(url["url"])
+            elif "error" in batch:
+                if "Results listing is limited to protect resources." in batch["error"]:
+                    logging.debug("Scraping stopped due to API_KEY access cap, to get around this you will need to upgrade your account")
+                    break
+                else:
+                    logging.debug("Unknown response received from server: {}".format(batch["error"]))
+            else:
+                logging.debug("No url in batch?")
+        except Exception as e:
+            logging.debug(str(e))
+
+def main():
+    # Parse Arguments
+    parser = argparse.ArgumentParser(description="Scraped shortned URLs for a given site from the GrayHatWarefare Shorteners API")
+    parser.add_argument('-t','--target', type=str, action="store", dest="target", help="Target domain/subdomains (e.g. example.com)", required=True)
+    parser.add_argument('-d','--debug', type=bool, action="store", dest="debug", help="Print debug information", default=False)
+    args = parser.parse_args()
+
+    # Setup logging
+    if args.debug:
+        root = logging.getLogger()
+        root.setLevel(logging.DEBUG)
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        root.addHandler(handler)
+
+    # Check if API key is setup, env var called GRAYHATWARFARE_API_KEY
+    if os.getenv("GRAYHATWARFARE_API_KEY"):
+        api_key = os.getenv("GRAYHATWARFARE_API_KEY")
+    else:
+        print("An API key is required to use this script, please register to obtain one at https://grayhatwarfare.com/register")
+        exit()
+
+    # setup debug logging if requested
+    if args.debug:
+        logging.basicConfig(filename='debug.log', level=logging.DEBUG)
+        logging.debug("Debug level set")
+    
+    # do the thing
+    get_urls(args.target, api_key)
+
+if __name__ == "__main__":
+    main()
